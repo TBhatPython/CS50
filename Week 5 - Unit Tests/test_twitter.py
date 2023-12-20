da0f1f7a4eeb41a7534267ea1bc9a9e298b06e02
TBhatPython <bhat.tushar@outlook.com>
2023-12-20 12:04:16 -0800

    test_twitter

--- Week 5 - Unit Tests/test_twitter.py
+++ Week 5 - Unit Tests/test_twitter.py
@@ -2,4 +2,5 @@ from twitter import shorten
 
 def test_shorten():
     assert shorten("twitter") == "twttr"
-
+    assert shorten("What's your name?") == "Wht's yr nm?"
+    assert shorten("CS50") == "CS50"
