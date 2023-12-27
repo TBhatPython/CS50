18ef9ecd0ad1642722a673cc5e8e98e6af35f1a5
TBhatPython <bhat.tushar@outlook.com>
2023-12-26 19:13:55 -0700

    um.py

---
+++ Week 7 - Regular Expressions/um.py
@@ -0,0 +1,14 @@
+import re
+
+def main():
+    print(count(input("Text: ")))
+
+
+def count(text):
+    matches = re.findall(r"\bum\b", text, flags=re.IGNORECASE)
+    return len(matches)
+
+
+
+if __name__ == "__main__":
+    main()
